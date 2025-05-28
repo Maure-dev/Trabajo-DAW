fada523c0096728e8f725b6b0762730996bc41e1
Brian Retamar <brianretamar0101@gmail.com>
2025-05-27 22:27:24 -0300

    feat: health check en /health

--- back/src/modules/surveys/surveys.module.ts
+++ back/src/modules/surveys/surveys.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SurveysController } from './controllers/surveys/surveys.controller';
+import { SurveysController } from './controllers/surveys.controller';
+import { HealthController } from './controllers/health.controller';
 import { SurveysService } from './services/surveys/surveys.service';
 
 @Module({
-  controllers: [SurveysController],
+  controllers: [SurveysController, HealthController],
   providers: [SurveysService],
 })
 export class SurveysModule {}
