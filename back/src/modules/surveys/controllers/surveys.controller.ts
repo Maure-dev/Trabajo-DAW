5ead042e224a912feabf09f352727dd6adfe6abd
Retamar Brian <leo1632@hotmail.com>
2025-06-02 00:11:58 -0300

    feat: add GET surveys endpoints

--- back/src/modules/surveys/controllers/surveys.controller.ts
+++ back/src/modules/surveys/controllers/surveys.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Param, Post, Get, Put, Query, ParseUUIDPipe } from '@nestjs/common';
 import { CreateSurveyDto } from '../dtos/create-survey.dto';
 import { UpdateSurveyDto } from '../dtos/update-survey.dto';
 import { SurveyResponseDto } from '../dtos/survey-response.dto';
 import { SurveysService } from '../services/surveys.service';
 import { plainToInstance } from 'class-transformer';
 import { Survey } from '../entities/survey.entity';
+import { GetSurveysQueryDto } from '../dtos/get-surveys-query.dto';
 
 @Controller('surveys')
 export class SurveysController {
@@ -13,6 +14,16 @@ export class SurveysController {
   @Post()
   create(@Body() createSurveyDto: CreateSurveyDto): Promise<Survey> {
     return this.surveysService.createSurvey(createSurveyDto);
+  }
+
+  @Get()
+  getByStatus (@Query() query: GetSurveysQueryDto): Promise<Survey[]> {
+    return this.surveysService.getSurveysByStatus(query.status);
+  }
+
+  @Get(':id')
+  getById (@Param('id', ParseUUIDPipe) id: string): Promise<Survey> {
+    return this.surveysService.getSurveyById(id);
   }
 
   @Put(':id')
